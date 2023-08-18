ba2dfa4fd2723b01e3f536e1e46eebb191cebeda
David Rendell <dairendell@gmail.com>
2023-08-18 10:13:29 +0100

    Merging

--- js-playground/codecademy-js-tutorial.js
+++ js-playground/codecademy-js-tutorial.js
@@ -8,3 +8,6 @@ console.log("Javascript tutorial");
     like this
 */
 console.log('First were sections on data types (int, string, bool etc.), operators and concatenatin - fine with this');
+console.log('Properties - data types like Strings all have properties which you can retrieve, such as string.length');
+console.log('You can use it to get the length of a string: ' + 'Hi Dave'.length);
+console.log('Methods!');
