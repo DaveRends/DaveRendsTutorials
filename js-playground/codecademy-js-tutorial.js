489d97a972da93363d4fe24685563391fde32110
David Rendell <dairendell@gmail.com>
2023-08-18 09:59:56 +0100

    Firt commit

---
+++ js-playground/codecademy-js-tutorial.js
@@ -0,0 +1,10 @@
+// Writing to the console
+console.log("Javascript tutorial");
+
+// Comments, and...
+/* 
+    Multiline comments
+    are done 
+    like this
+*/
+console.log('First were sections on data types, operators and concatenatin - fine with this');
