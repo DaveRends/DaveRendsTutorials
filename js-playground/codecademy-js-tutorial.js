1e19a8be1ad3817469044b17bc4a6da3adc478ab
David Rendell <dairendell@gmail.com>
2023-08-18 10:16:21 +0100

    Commit friday branch

--- js-playground/codecademy-js-tutorial.js
+++ js-playground/codecademy-js-tutorial.js
@@ -1,6 +1,6 @@
 // Writing to the console
 console.log("Javascript tutorial");
-
+console.log("Run this tutorial while reading the code for brief explanations of all things Java!");
 // Comments, and...
 /* 
     Multiline comments
@@ -8,3 +8,6 @@ console.log("Javascript tutorial");
     like this
 */
 console.log('First were sections on data types (int, string, bool etc.), operators and concatenatin - fine with this');
+console.log('Properties - data types like Strings all have properties which you can retrieve, such as string.length');
+console.log('You can use it to get the length of a string: ' + 'Hi Dave'.length);
+console.log('Methods!');
